010779fb3c423d9f59a48a61503583bfeced0658
homi33 <homi9800@gmail.com>
2022-09-20 21:19:20 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about</h1>
+      <p>
+        Serving a cocktail before dinner is a courtesy to guests but it also
+        serves a purpose. Known as an aperitif, the before-dinner drink prepares
+        the stomach for food and the palate for the delicious tastes it is about
+        to enjoy. Cocktails typically have a base liquor, like vodka or gin, and
+        come with other flavored ingredients like fruit juice. Some drinks are
+        simple and only involve two liquid ingredients. Other cocktails are more
+        complex, involving a base liquor, several fruit juices, spices, and a
+        garnish.
+      </p>
+    </section>
+  );
+}
